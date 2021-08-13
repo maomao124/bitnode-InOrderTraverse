5e6ac506962924d8cffaf469ef7ac36a55bd16a7
毛 <1296193245@qq.com>
2021-08-13 20:46:56 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,412 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include <Windows.h>
+#include<time.h>
+#include <graphics.h>
+#include <conio.h>
+#include<algorithm>
+#include<stdlib.h>
+#include <random>
+#include <string>
+#include <fstream>
+#include <ddraw.h>
+#include<iomanip>
+#include <Psapi.h>
+#include<vector>
+#include<io.h>
+#include <sapi.h>                                             // 语音头文件
+#include <sphelper.h>                                     // 语音识别头文件
+#include <thread>                                           //多线程
+                                                                        //vs2019自定义控制台代码模板 by mao
+                                                                        //作者QQ1296193245
+using namespace std;
+#pragma warning(disable : 4996)
+#pragma comment (lib,"ddraw.lib")
+#pragma comment(lib,"sapi.lib")
+#pragma comment(lib,"ole32.lib")
+#pragma intrinsic(__rdtsc)
+unsigned __int64 t1, t2, t3, t4;
+double run_time;                                                   //微秒级算法时间计时器
+LARGE_INTEGER time_start;	                                //开始时间
+LARGE_INTEGER time_over;	                                //结束时间
+double dqFreq;		                                               //计时器频率
+LARGE_INTEGER f;	                                               //计时器频率
+double tt;
+long double totaltime;
+
+inline void runstart()
+{
+	QueryPerformanceFrequency(&f);
+	dqFreq = (double)f.QuadPart;
+	t1 = __rdtsc();
+	QueryPerformanceCounter(&time_start);	       //计时开始
+}
+inline void runend()
+{
+	QueryPerformanceCounter(&time_over);	        //计时结束
+	run_time = 1000 * (time_over.QuadPart - time_start.QuadPart) / dqFreq;
+	t2 = __rdtsc();
+	t3 = t2 - t1;
+}
+void ShowProcessMemoryUsageInfo()
+{
+	HANDLE handle = GetCurrentProcess();
+	PROCESS_MEMORY_COUNTERS pmc;
+	GetProcessMemoryInfo(handle, &pmc, sizeof(pmc));
+	float memoryUsage_M = pmc.WorkingSetSize / (1024.0 * 1024.0);
+	float memoryUsage_K = pmc.WorkingSetSize / 1024.0;
+	std::cout << std::fixed << setprecision(0) << "程序内存使用率：" << memoryUsage_K << " KB = " << setprecision(2) << memoryUsage_M << " MB" << std::endl;
+}
+__int64 CompareFileTime(FILETIME time1, FILETIME time2)//转换为整数时间
+{
+	__int64 a = static_cast<long long>(time1.dwHighDateTime) << 32 | time1.dwLowDateTime;
+	__int64 b = static_cast<long long>(time2.dwHighDateTime) << 32 | time2.dwLowDateTime;
+	return   (b - a);
+}
+void rundisplay()
+{
+	cout << endl;
+	cout << "-------------------------------------------------------" << endl;
+	if (t3 < 10000)
+	{
+		cout << "CPU脉冲数：" << (t3) << endl;
+	}
+	else if (t3 < 100000000 && t3 >= 10000)
+	{
+		tt = t3;
+		cout << "CPU脉冲数：" << (tt) / 10000 << "万" << endl;
+	}
+	else if (t3 > 100000000000)
+	{
+		t4 = t3;
+		t4 = t3 / 10000000;
+		tt = t4;
+		cout << "CPU脉冲数：" << (tt) / 10 << "亿" << endl;
+	}
+	else
+	{
+		t4 = t3;
+		t4 = t3 / 10000;
+		tt = t4;
+		cout << "CPU脉冲数：" << (tt) / 10000 << "亿" << endl;
+	}
+	if (run_time > 10000)
+	{
+		cout << "算法运行时间：" << run_time / 1000 << "秒" << endl;
+	}
+	else if (run_time < 1)
+	{
+		cout << "算法运行时间：" << run_time * 1000 << "微秒" << endl;
+	}
+	else
+	{
+		cout << "算法运行时间：" << run_time << "毫秒" << endl;
+	}
+	totaltime = clock();
+	cout << "程序运行时间：" << totaltime / 1000 << "秒" << endl;
+	if (run_time > 0.4)
+	{
+		double frequency = t3 / run_time / 1000;
+		cout << "CPU频率：" << frequency << "MHZ" << endl;
+	}
+	ShowProcessMemoryUsageInfo();
+	long lRet;
+	HKEY hKey;
+	TCHAR tchData[64];
+	DWORD dwSize;
+	lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Hardware\\Description\\System\\CentralProcessor\\0", 0, KEY_QUERY_VALUE, &hKey);
+	if (lRet == ERROR_SUCCESS)
+	{
+		dwSize = sizeof(tchData);
+		lRet = RegQueryValueEx(hKey, L"ProcessorNameString", NULL, NULL, (LPBYTE)tchData, &dwSize);
+		if (lRet == ERROR_SUCCESS)
+		{
+			cout << "CPU 名字：";
+			wcout << tchData << endl;
+		}
+		else
+		{
+			cout << "CPU 名字：";
+			wcout << L"未知" << endl;
+		}
+	}
+	else
+	{
+		cout << "CPU 名字：";
+		wcout << L"未知" << endl;
+	}
+	RegCloseKey(hKey);
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+	double totalphy = statex.ullTotalPhys / 1024 / 1024;
+	totalphy = totalphy / 1024;
+	cout << "总物理内存：    " << setprecision(4) << left << setw(7) << totalphy << " GB" << endl;
+	double totalvir = statex.ullTotalPageFile / 1024 / 1024;
+	totalvir = totalvir / 1024;
+	cout << "总虚拟内存：    " << setw(7) << totalvir - totalphy << " GB" << endl;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coordScreen = { 0, 0 }; //光标位置
+	CONSOLE_SCREEN_BUFFER_INFO csbi{};
+	if (GetConsoleScreenBufferInfo(hConsole, &csbi))
+	{
+		//printf("光标坐标:(%d,%d)\n", csbi.dwCursorPosition.X, csbi.dwCursorPosition.Y);
+	}
+	HANDLE   hStdout;
+	//   光标位置   
+	COORD     cursorPos;
+	//   标准输出句柄   
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE hEvent;
+	BOOL res;
+	FILETIME preidleTime;
+	FILETIME prekernelTime;
+	FILETIME preuserTime;
+	FILETIME idleTime;
+	FILETIME kernelTime;
+	FILETIME userTime;
+	res = GetSystemTimes(&idleTime, &kernelTime, &userTime);
+	preidleTime = idleTime;
+	prekernelTime = kernelTime;
+	preuserTime = userTime;
+	hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);// 初始值为 nonsignaled ，并且每次触发后自动设置为nonsignaled
+	double freephy = statex.ullAvailPhys / 1024 / 1024;
+	freephy = freephy / 1024;
+	double usephy = (totalphy - freephy) * 100.0 / totalphy;
+	cout << "内存使用率：    " << setprecision(3) << usephy << "%" << endl;
+	double freetotal = statex.ullAvailPageFile / 1024 / 1024;
+	freetotal = freetotal / 1024;
+	cout << "总空闲内存：    " << setprecision(3) << freetotal << " GB" << endl;
+	double freevir = (totalvir - freetotal) - (totalphy * statex.dwMemoryLoad / 100.0);
+	cout << "虚拟内存使用量：" << setprecision(3) << freevir << " GB" << endl;
+	double usevir = freevir * 100 / (totalvir - totalphy);
+	cout << "虚拟内存使用率：";
+	printf("%.2f%%", usevir);
+	cout << endl;
+	cout << "CPU利用率：    " << right << setprecision(2) << setw(6) << 0.00 << "%" << endl;
+	cout << "CPU空闲率：    " << setw(6) << 100.0<< "%" << endl;
+	cout << "-------------------------------------------------------" << endl;
+	cout << "按任意键退出程序" << endl;
+	while (!_kbhit())
+	{
+		GlobalMemoryStatusEx(&statex);
+		WaitForSingleObject(hEvent, 1000);
+		res = GetSystemTimes(&idleTime, &kernelTime, &userTime);
+		int idle = CompareFileTime(preidleTime, idleTime);
+		int kernel = CompareFileTime(prekernelTime, kernelTime);
+		int user = CompareFileTime(preuserTime, userTime);
+		float cpu = (kernel + user - idle) * 100.0 / (kernel + user);
+		float cpuidle = (idle) * 100.0 / (kernel + user);
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 0;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		//cout << "内存使用率：" << statex.dwMemoryLoad << "%" << endl;
+		double freephy = statex.ullAvailPhys / 1024 / 1024;
+		freephy = freephy / 1024;
+		double usephy = (totalphy - freephy) * 100.0 / totalphy;
+		cout << "内存使用率：    " << setprecision(3) << usephy << "%" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 1;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		double freetotal = statex.ullAvailPageFile / 1024 / 1024;
+		freetotal = freetotal / 1024;
+		cout << "总空闲内存：    " << setprecision(3) << freetotal <<" GB" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 2;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		double freevir = (totalvir - freetotal) - (totalphy * statex.dwMemoryLoad / 100.0);
+		cout << "虚拟内存使用量：" << setprecision(3) << freevir <<" GB" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 3;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		double usevir = freevir * 100 / (totalvir - totalphy);
+		cout << "虚拟内存使用率：";
+		printf("%.2f%%", usevir);
+		cout << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 4;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "CPU利用率：    " << right << setprecision(2) << setw(6) << cpu << "%" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 5;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "CPU空闲率：    " << setw(6) << cpuidle << "%"<<endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 6;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "-------------------------------------------------------" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 7;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "按任意键退出程序" << endl;
+		preidleTime = idleTime;
+		prekernelTime = kernelTime;
+		preuserTime = userTime;
+	}
+	cout << endl;
+	
+}
+
+
+//**************************************************************//************************非主函数
+
+#define TElemType int
+
+typedef struct BiTNode
+{
+	TElemType data;//数据域
+	struct BiTNode* lchild, * rchild;//左右孩子指针
+}BiTNode, * BiTree;
+
+//初始化树的函数
+void CreateBiTree(BiTree* T) 
+{
+	*T = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->data = 1;
+	(*T)->lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->lchild->data = 2;
+	(*T)->lchild->lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->lchild->rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->lchild->rchild->data = 5;
+	(*T)->lchild->rchild->lchild = NULL;
+	(*T)->lchild->rchild->rchild = NULL;
+	(*T)->rchild->data = 3;
+	(*T)->rchild->lchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->rchild->lchild->data = 6;
+	(*T)->rchild->lchild->lchild = NULL;
+	(*T)->rchild->lchild->rchild = NULL;
+	(*T)->rchild->rchild = (BiTNode*)malloc(sizeof(BiTNode));
+	(*T)->rchild->rchild->data = 7;
+	(*T)->rchild->rchild->lchild = NULL;
+	(*T)->rchild->rchild->rchild = NULL;
+	(*T)->lchild->lchild->data = 4;
+	(*T)->lchild->lchild->lchild = NULL;
+	(*T)->lchild->lchild->rchild = NULL;
+}
+
+void displayElem(BiTNode* elem)
+{
+	printf("%d ", elem->data);
+}
+//中序遍历递归
+void INOrderTraverse(BiTree T) 
+{
+	if (T)
+	{
+		INOrderTraverse(T->lchild);
+		displayElem(T);
+		INOrderTraverse(T->rchild);
+	}
+	                      //如果结点为空，返回上一层
+	return;
+}
+
+int top = -1;
+void push(BiTNode** a, BiTNode* elem)
+{
+	a[++top] = elem;
+}
+//弹栈函数
+void pop()
+{
+	if (top == -1)
+	{
+		return;
+	}
+	top--;
+}
+
+BiTNode* getTop(BiTNode** a) 
+{
+	return a[top];
+}
+
+//中序遍历非递归算法
+void InOrderTraverse1(BiTree Tree)
+{
+	BiTNode* a[20];//定义一个顺序栈
+	BiTNode* p;//临时指针
+	push(a, Tree);//根结点进栈
+	while (top != -1) {//top!=-1说明栈内不为空，程序继续运行
+		while ((p = getTop(a)) && p) {//取栈顶元素，且不能为NULL
+			push(a, p->lchild);//将该结点的左孩子进栈，如果没有左孩子，NULL进栈
+		}
+		pop();//跳出循环，栈顶元素肯定为NULL，将NULL弹栈
+		if (top != -1)
+		{
+			p = getTop(a);//取栈顶元素
+			pop();//栈顶元素弹栈
+			displayElem(p);
+			push(a, p->rchild);//将p指向的结点的右孩子进栈
+		}
+	}
+}
+//中序遍历实现的另一种方法
+void InOrderTraverse2(BiTree Tree)
+{
+	BiTNode* a[20];//定义一个顺序栈
+	BiTNode* p;//临时指针
+	p = Tree;
+	//当p为NULL或者栈为空时，表明树遍历完成
+	while (p || top != -1) 
+	{
+		//如果p不为NULL，将其压栈并遍历其左子树
+		if (p)
+		{
+			push(a, p);
+			p = p->lchild;
+		}
+		//如果p==NULL，表明左子树遍历完成，需要遍历上一层结点的右子树
+		else
+		{
+			p = getTop(a);
+			pop();
+			displayElem(p);
+			p = p->rchild;
+		}
+	}
+}
+
+//**************************************************************//************************
+
+int main()
+{
+
+	//*************************************************************************
+
+
+
+	//*************************************************************************
+	runstart();
+	//*************************计时开始***************************************
+	
+	BiTree Tree;
+	CreateBiTree(&Tree);
+	printf("中序遍历递归: \n");
+	INOrderTraverse(Tree);
+
+	BiTree Tree2;
+	CreateBiTree(&Tree2);
+	printf("\n中序遍历非递归算法1: \n");
+	InOrderTraverse1(Tree2);
+	printf("\n中序遍历非递归算法2: \n");
+	InOrderTraverse2(Tree2);
+
+	//*************************计时结束***************************************
+	runend();
+
+
+
+	
+
+
+
+	rundisplay();                                                                   //计时结果显示
+	_getch();
+	//closegraph();
+	// system("pause");
+	//Sleep(10000);
+	return 0;
+}
